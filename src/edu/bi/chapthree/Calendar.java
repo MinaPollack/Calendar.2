b4cecc24bc4a745502e1650f90fc90307c76bb18
MinaPollack <mina.pollack@ti.htc.edu>
2020-12-02 17:31:42 -0800

    created a header added more lines of calendar

--- src/edu/bi/chapthree/Calendar.java
+++ src/edu/bi/chapthree/Calendar.java
@@ -18,6 +18,46 @@ public class Calendar {
 
     //creates the calendar
     public static void printCalendar(int date, int days) {
+        printHeader();
+
+        //prints the first week
+        System.out.print("|  ");
+        for (int t = 7; t >= 7; t--) {
+            int i = (date - 6);
+            for (int j = i; j <= 1; j++) {
+                System.out.print("    |  ");
+            }
+        }
+
+        for (int l = date; l > 1; l--) {
+            int k = date;
+            System.out.print(" " + ((k + 1) - l) + "  |  ");
+        }
+        System.out.println();
+        //prints body weeks
+        System.out.print("|  ");
+            for (int x = date; x < 10; x++) {
+                System.out.print(" " + x + "  |  ");
+                for (int w = x; w >= (date + 7); w--) {
+                    System.out.println();
+                    System.out.print("|   " + x + "  |  ");
+                }
+            }
+
+            for (int y = 10; y <= days; y++) {
+                System.out.print(y + "  |  ");
+            }
+            System.out.println();
+            System.out.println("|  ");
+
+        //closes the calendar
+        for (int z = 1; z <= 7; z++) {
+            System.out.print("+------");
+        }
+        System.out.println("+");
+    }
+
+    private static void printHeader() {
         System.out.print("  Sun  ");
         System.out.print("  Mon  ");
         System.out.print("  Tue  ");
@@ -26,24 +66,6 @@ public class Calendar {
         System.out.print("  Fri  ");
         System.out.println("  Sat  ");
         for (int i = 1; i <= 7; i++) {
-            System.out.print("+------");
-        }
-        System.out.println("+");
-        System.out.print("|  ");
-        for (int t = 7; t >= date; t--) {
-            for (int i = date - 6; i <= 1; i++) {
-                System.out.print(date - t + "  |  ");
-            }
-            System.out.println();
-            System.out.print("|  ");
-        }
-        for (int y = date; y < 10; y++) {
-            System.out.print(y + "  |  ");
-        }
-        for (int z = 10; z <= days; z++) {
-            System.out.print(z + "  |  ");
-        }
-        for (int x = 1; x <= 7; x++) {
             System.out.print("+------");
         }
         System.out.println("+");
@@ -56,6 +78,6 @@ public class Calendar {
         System.out.println("and the date of the first Sunday.");
         System.out.println("The number of days can be from 28 to 31,");
         System.out.println("and the date of the first Sunday an be from 1 to 7");
-        System.out.println("");
+        System.out.println();
     }
 }
